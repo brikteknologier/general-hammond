ef084198c5243f426de04b74b7531dc748b9a904
Jon Packer <contact@jonpacker.com>
2013-10-09 21:53:16 +0200

    Add tests for new api format

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 process.argv = ['node', 'thing.js', '--config', './test/test-config.json'];
 var assert = require('assert');
 var hammond = require('../');
+var http = require('http');
 describe('George Hammond', function() {
   it('should read a config file', function() {
     var config = require('../')('service');
@@ -11,13 +12,29 @@ describe('George Hammond', function() {
       var config = hammond('service', ['someCrazyNewThing'])(function(cfg){});
     });
   });
-  it('should inherit values', function() {
+  it('should inherit values', function(done) {
     hammond('service')(function(config) {
       assert(config.globalSetting);
+      done()
     });
   });
-  it('should get other services', function() {
-    var config = hammond('service');
-    assert(config.otherService == 'http://localhost:1234');
+  it('should get other services', function(done) {
+    hammond('service')(function(config) {
+      assert(config.otherService == 'http://localhost:1234');
+      done()
+    });
+  });
+  it('should retrieve a config from an arbitrary location', function(done) {
+    var server = http.createServer(function(req,res) {
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(200);
+      var confstream = require('fs').createReadStream('./test/test-config.json');
+      confstream.pipe(res);
+    }).listen(62853);
+    process.argv = ['node', 'thing.js', '--config', 'http://localhost:62853'];
+    hammond('service')(function(config) {
+      assert(config.globalSetting);
+      done();
+    });
   });
 });
